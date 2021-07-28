01464365cf984816e3a0c17b1cdf6d77c8d102ac
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-07-28 23:22:27 +0300

    Add files via upload

---
+++ C# OOP/0.DefiningClasses/2.DefiningClasses-Exercise/InheritanceConstructors/FrontEndProgrammer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceConstructors
+{
+    class FrontEndProgrammer : Programmer
+    {
+        public FrontEndProgrammer(string name, int salary)
+            :base(name, salary, new List<string> { "JavaScript", "React"})
+        {
+            Console.WriteLine("In FrontEnd");
+        }
+    }
+}
---
+++ C# OOP/0.DefiningClasses/2.DefiningClasses-Exercise/InheritanceConstructors/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InheritanceConstructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FrontEndProgrammer frontEnd = new FrontEndProgrammer("Bai Ti Pesho", 200);
+        }
+    }
+}
