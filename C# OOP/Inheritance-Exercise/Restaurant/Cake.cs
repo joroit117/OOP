ad1bcd78d7e10cfbc4cbd9346b470f54c1844eb9
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-08-04 22:27:25 +0300

    Add files via upload

---
+++ C# OOP/Inheritance-Exercise/Restaurant/Cake.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        private const double grams = 250;
+        private const double calories = 1000;
+        private const decimal cakePrice = 5;
+
+        public Cake(string name, decimal price, double grams, double calories)
+            : base(name, 0, 0, 0)
+        {
+            Name = name;
+        }
+
+        public override double Grams
+            => grams;
+        public override double Calories
+            => calories;
+        public override decimal Price
+            => cakePrice;
+    }
+}
