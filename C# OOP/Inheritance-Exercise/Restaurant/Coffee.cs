da39b116f71682612996d435bbc24c721a6e29d8
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-08-04 23:19:28 +0300

    Add files via upload

---
+++ C# OOP/Inheritance-Exercise/Restaurant/Coffee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : Beverage
+    {
+        private const double DefaultMilliliters = 50;
+        private const decimal DefaultPrice = 3.5M;
+        public Coffee(string name, double caffeine)
+            : base(name, DefaultPrice, DefaultMilliliters)
+        {
+            Caffeine = caffeine;
+        }
+
+        public double Caffeine { get; set; }
+    }
+}
