2c191d036f5470804123a64cc8f0c843c200f64b
Georgi Ivanov <78050030+joroit117@users.noreply.github.com>
2021-08-05 12:11:03 +0300

    Add files via upload

---
+++ C# OOP/Encapsulation/PersonsInfo/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            
+            var persons = new List<Person>();
+
+            var lines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                var person = new Person(cmdArgs[0],
+                cmdArgs[1],
+                int.Parse(cmdArgs[2]),
+                decimal.Parse(cmdArgs[3]));
+                persons.Add(person);
+            }
+            Team team = new Team("SoftUni");
+
+            persons.ForEach(p => team.AddPlayer(p));
+
+            Console.WriteLine($"Frist team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+
+        }
+    }
+}
